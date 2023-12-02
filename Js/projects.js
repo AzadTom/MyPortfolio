2cf2009a15329ed95214a50bcac405874e842275
azadtom <kumarazad2917@gmail.com>
2023-12-02 10:26:01 +0530

    content arragned

--- Js/projects.js
+++ Js/projects.js
@@ -7,15 +7,15 @@ const webContainer = document.getElementById("webContainers");
 let  webContent = [{
     img : "./img/ecommerceb.png",
     name : "Ecommerce Webapplication",
-    overview : " This application is built with React/conextApi and mockBackend with miragJs & tailwindCss",
-    description :"I implemented feature such as authentication/landingPage and cart feature & favourite  and deatail page as well.",
+    // overview : " This application is built with React/conextApi and mockBackend with miragJs & tailwindCss",
+    // description :"I implemented feature such as authentication/landingPage and cart feature & favourite  and deatail page as well.",
     live:"https://boldx.netlify.app/"
 },
 {
     img : "./img/neetflix.png",
     name : "Neetflix website",
-    overview : "  This application is built with React and used rest api from Tmdb database.",
-    description :"I implemented home page with different genre of movie show when page is loaded..",
+    // overview : "  This application is built with React and used rest api from Tmdb database.",
+    // description :"I implemented home page with different genre of movie show when page is loaded..",
     live:"https://neetflixo.netlify.app/"
 }]
 
@@ -24,41 +24,41 @@ let  webContent = [{
 let androidContent = [{
   img : "./img/ecommerce.png",
   name : "Ecommerce Webapplication",
-  overview : "  This app used Jetpack compose for UI & Kotlin and Firebase for backend ",
+  // overview : "  This app used Jetpack compose for UI & Kotlin and Firebase for backend ",
   live:"https://github.com/AzadTom/E_commerce/"
 },
 {
   img : "./img/movie.png",
   name : "Movie Application",
-  overview : ` This project is based on TMDB API using retrofit to fetch a
-  list of data display on homescreen where you see a lot of
-  movie after clicking you will go to other screen called
-  detailscreen and also i used pagination & swipeRefreshLayoutd `,
+  // overview : ` This project is based on TMDB API using retrofit to fetch a
+  // list of data display on homescreen where you see a lot of
+  // movie after clicking you will go to other screen called
+  // detailscreen and also i used pagination & swipeRefreshLayoutd `,
   live:"https://github.com/AzadTom/MDN-Pagination"
 },
 {
   img : "./img/Chat.png",
   name : "Chat Application",
-  overview : "   This app used XML for UI & Kotlin and Firebase for backend",
+  // overview : "   This app used XML for UI & Kotlin and Firebase for backend",
   live:"https://github.com/AzadTom/Boss-ChatApplication-"
 },
 {
   img : "./img/News.png",
   name : "News Application",
-  overview : "    This app used XML for UI & Kotlin and Firebase for backend",
+  // overview : "    This app used XML for UI & Kotlin and Firebase for backend",
   live:"https://github.com/AzadTom/BrowseNews"
 },
 {
   img : "./img/expens.png",
   name : "Expenses  Application",
-  overview : "This app used XML for UI & Kotlin and Local storage as database implemented by Room Library ",
+  // overview : "This app used XML for UI & Kotlin and Local storage as database implemented by Room Library ",
   live:"https://github.com/AzadTom/Android-AddBill_App"
 },
 
 {
   img : "./img/reminder.png",
   name : "Reminder Application",
-  overview : "  This app used XML for UI & Kotlin and Local storage as database and implemented AlarmManager as well",
+  // overview : "  This app used XML for UI & Kotlin and Local storage as database and implemented AlarmManager as well",
   live:"https://github.com/AzadTom/TODOReminder"
 },]
 
@@ -87,12 +87,11 @@ function webContentRender(content){
               </div>
       
               <p>
-              ${item.overview}
+              ${item.overview === undefined ? "":item.overview}
       
               </p>
-              <p>${item.description}</p>
-              <a href=${item.live} id="read-more" target="_blank"
-                >Live link</a
+              <p>${item.description===undefined? "":item.description}</p>
+              <a href="${item.live}" id="read-more" target="_blank">Live link</a
               >
             </div>
            
@@ -139,10 +138,10 @@ function androidContentRender(content){
             </div>
     
             <p>
-            ${item.overview}
+            ${item.overview===undefined? "":item.overview}
     
             </p>
-            <a href=${item.live} id="read-more" target="_blank"
+            <a href="${item.live}" id="read-more" target="_blank"
               >Live link</a
             >
           </div>
